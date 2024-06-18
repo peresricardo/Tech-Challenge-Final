561ed6f70ef36b84a526e7defa10ee16296ca6ed
Warley Araujo <warley1955@gmail.com>
2024-06-18 18:47:43 -0300

    subindo o servico inicial de pagamento

---
+++ srvPagamento/src/main/java/br/com/fiap/srvpagamento/SrvPagamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.srvpagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvPagamentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrvPagamentoApplication.class, args);
+    }
+
+}
